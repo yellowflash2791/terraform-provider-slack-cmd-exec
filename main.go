3ccf252271e01bd401e9792f85bc169a0d10f247
yellowflash2791 <anthonyarnold@Anthonys-MacBook-Pro.local>
2023-08-31 16:09:56 +0800

    add proper import in main.go 2

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/yellowflash2791/slack_exec/"
+	"github.com/yellowflash2791/slack_exec"
 )
 
 func main() {
