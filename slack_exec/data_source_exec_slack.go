9b4e18c9adcb20698ef7f21bd36172992a64ff71
yellowflash2791 <anthonyarnold@Anthonys-MacBook-Pro.local>
2023-08-31 16:26:34 +0800

    comment github.com/hashicorp/terraform-plugin-sdk/v2/plugin in data_source_exec_slack.go

--- slack_exec/data_source_exec_slack.go
+++ slack_exec/data_source_exec_slack.go
@@ -8,7 +8,6 @@ import (
 	"os/exec"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-//	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func dataSourceSlackNotify() *schema.Resource {
